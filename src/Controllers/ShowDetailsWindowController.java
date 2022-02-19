d1b23f4bd19535881ac50f62662c3a964b796ec8
MichalskiWiktor <wiktor.kasa1@gmail.com>
2022-02-19 13:19:19 +0100

    show window redesign

--- src/Controllers/ShowDetailsWindowController.java
+++ src/Controllers/ShowDetailsWindowController.java
@@ -6,9 +6,7 @@ import javafx.scene.control.Label;
 
 public class ShowDetailsWindowController {
     private Task task;
-    @FXML private Label idLabel;
     @FXML private Label priorityLabel;
-    @FXML private Label statusLabel;
     @FXML private Label titleLabel;
     @FXML private Label descriptionLabel;
     @FXML private Label dateLabel;
@@ -17,15 +15,9 @@ public class ShowDetailsWindowController {
         this.loadDataToLabels();
     }
     private void loadDataToLabels(){
-        this.idLabel.setText(this.idLabel.getText() + " " + this.task.getId());
-
         if(this.task.getPriority()==1)this.priorityLabel.setText(this.priorityLabel.getText() + " Low");
         else if(this.task.getPriority()==2)this.priorityLabel.setText(this.priorityLabel.getText() + " Medium");
         else if(this.task.getPriority()==3)this.priorityLabel.setText(this.priorityLabel.getText() + " High");
-
-        if(this.task.getStatus()==1)this.statusLabel.setText(this.statusLabel.getText() + " To do");
-        else if(this.task.getStatus()==2)this.statusLabel.setText(this.statusLabel.getText() + " In progress");
-        else if(this.task.getStatus()==3)this.statusLabel.setText(this.statusLabel.getText() + " Done");
 
         this.titleLabel.setText(this.titleLabel.getText() + " " + this.task.getTitle());
         this.descriptionLabel.setText(this.descriptionLabel.getText() + " " + this.task.getDescription());
